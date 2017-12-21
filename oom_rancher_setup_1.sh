8f38192598cffa1332bf4c964b06be72fd971042
Michael O'Brien <michael@obrienlabs.org>
2017-12-21 18:11:47 -0500

    #13 1.8.4 until I triage issues with SDNC under Kubernetes 1.8.6

--- oom_rancher_setup_1.sh
+++ oom_rancher_setup_1.sh
@@ -6,7 +6,7 @@ curl https://releases.rancher.com/install-docker/1.12.sh | sh
 docker run -d --restart=unless-stopped -p 8880:8080 rancher/server:v1.6.10
 # no Kubernetes 1.9.0 yet
 #curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.6/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.4/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 mkdir ~/.kube
