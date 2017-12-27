e16f216637acf369060b63fcc8e921047fcdaf22
Michael O'Brien <michael@obrienlabs.org>
2017-12-27 13:40:38 -0500

    #14 use google gerrit.googlesource.com/git-repo

--- oom_rancher_setup_1.sh
+++ oom_rancher_setup_1.sh
@@ -3,10 +3,10 @@
 curl https://releases.rancher.com/install-docker/1.12.sh | sh
 # when running as non-root (ubuntu) run the following and logout/log back in
 #sudo usermod -aG docker ubuntu
-docker run -d --restart=unless-stopped -p 8880:8080 rancher/server:v1.6.10
+docker run -d --restart=unless-stopped -p 8880:8080 --name rancher-server rancher/server:v1.6.10
 # no Kubernetes 1.9.0 yet
 #curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.4/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.6/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 mkdir ~/.kube
